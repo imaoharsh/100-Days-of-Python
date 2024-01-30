747ef50099f00f3428f692a6c3385a4bbccbdc8e
Harshit <101876400+imaoharsh@users.noreply.github.com>
2024-01-30 22:48:29 +0530

    adding list comprehension

---
+++ List_Comprehension.py
@@ -0,0 +1,6 @@
+lst=[1,2,3,4,5,6]
+print(lst)
+
+lst=[i**2 for i in lst]  #this is called list comprehension in which we write what operation we have to perform for loop and then as for loop
+# iterate we store value inside lst.
+print(lst)
